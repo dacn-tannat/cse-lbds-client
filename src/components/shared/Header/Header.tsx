cbf346cfbb499e4c926f5c15902db25b0d609494
Gemnyx <93497559+Gemnyx@users.noreply.github.com>
2025-02-09 22:51:59 +0700

    Fix UI (Homepage, Header)
    Add login with Google. Update Navbar.

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,13 +1,25 @@
-import { Home } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Home, LogOut } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className='border-b p-2 sticky z-50 top-0 bg-gray-100'>
-      <div className='container mx-auto px-4 py-2 flex justify-between items-center'>
-        <Link to='/' className='text-xl font-semibold'>
-          <Home />
-        </Link>
+      <div className='max-w-7xl min-w-xl mx-auto px-4 py-2 flex justify-between items-center'>
+        <div className='flex gap-10'>
+          <Link to='/' className='text-md md:text-lg lg:text-xl font-semibold text-gray-700 hover:text-black'>
+            <Home />
+          </Link>
+          <Link to='/problems' className='text-md md:text-lg lg:text-xl font-normal text-gray-700 hover:text-black'>
+            <span>Problem List</span>
+          </Link>
+        </div>
+        <Button variant="link" className="hover:no-underline !text-gray-700 hover:!text-black transition-colors duration-200">
+          <div className="flex gap-2 items-center text-md md:text-lg lg:text-xl">
+            <LogOut className="text-inherit" />
+            <span className="font-normal text-inherit">Log out</span>
+          </div>
+        </Button>
       </div>
     </header>
   )
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,14 +6,22 @@ import { Button } from '@/components/ui/button'
 export default function HomePage() {
   const navigate = useNavigate()
   return (
-    <div className='bg-gray-50 flex min-h-screen flex-col items-center justify-center'>
-      <div className='text-6xl font-bold mb-8'>Welcome to CSE-LBDS</div>
-      <div className='text-2xl font-normal mb-10 text-gray-500 italic'>
+    <div className='bg-gray-50 flex min-h-screen flex-col items-center justify-center px-4 text-center'>
+      <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 md:mb-8">
+        Welcome to CSE-LBDS
+      </div>
+      <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-normal mb-6 sm:mb-8 md:mb-10 text-gray-500 italic px-5 lg:px-24">
         Hệ thống phát hiện và gợi ý sửa lỗi logic cho các bài tập trong khoá học Thực hành - Kỹ thuật Lập trình
       </div>
-      <Button variant='link' className='text-xl italic font-normal text-blue-500' onClick={() => navigate('/problems')}>
+      {/* <Button variant='link' className='text-xl italic font-normal text-blue-500' onClick={() => navigate('/problems')}>
         <ChevronsRight />
         Danh sách bài tập
+      </Button> */}
+      <Button className="hover:bg-gray-400 rounded-xl text-lg px-6 py-3 sm:px-8 sm:py-4 md:px-10 md:py-5 shadow bg-gray-200">
+        <div className="flex gap-3 sm:gap-5 items-center">
+          <img src="src/assets/google.png" className="size-6" />
+          <span className="text-sm sm:text-base md:text-lg">Đăng nhập với Google</span>
+        </div>
       </Button>
     </div>
   )
