e0faa286f3b4fee0f08ae3bcefffe3a4b88661ec
Gemnyx <93497559+Gemnyx@users.noreply.github.com>
2025-02-16 22:16:05 +0700

    Add Error Message

--- src/pages/ProblemDetail/ProblemDetail.tsx
+++ src/pages/ProblemDetail/ProblemDetail.tsx
@@ -4,6 +4,9 @@ import { problems } from '@/mockData/problems'
 import ProblemInformation from './components/ProblemInformation'
 import CodeEditor from './components/CodeEditor'
 import { generateSlug } from '@/utils/slug'
+import { useState } from 'react'
+import { SubmissionResponse } from '@/types'
+import ErrorMessage from './components/ErrorMessage'
 
 export default function ProblemDetail() {
   /* Problem */
@@ -11,12 +14,19 @@ export default function ProblemDetail() {
   // temporary mock data --> fetch problem from db by id
   const problem = problems.find((p) => generateSlug(p.name) === slug)
 
+  const [response, setResponse] = useState<SubmissionResponse | null>(null)
+  console.log(response)
+
   if (problem) {
     return (
+      
       <div className='bg-gray-50'>
+        {/* {response && <div>{JSON.stringify(response)}</div>} */}
         <div className='max-w-7xl min-h-screen mx-auto p-8'>
           <ProblemInformation problem={problem} />
-          <CodeEditor problem_id={problem.id} />
+          <CodeEditor problem_id={problem.id} setResponse={setResponse} />
+          {response && response.message !== 'Accepted' && <ErrorMessage message={response.message} status={response.status} />}
+          
         </div>
       </div>
     )
---
+++ src/utils/status.ts
@@ -0,0 +1,9 @@
+export const statusMapping: Record<number, string> = {
+  0: "Compile error",
+  1: "Wrong answer",
+  2: "Time limit",
+  4: "Accepted",
+  6: "Output limit",
+  7: "Runtime error",
+  8: "Presentation error"
+}
