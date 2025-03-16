e7ad6b7502146cf410e816c8b961673545e81859
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-03-16 11:32:49 +0700

    fix: add config google redirect uri for production

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,5 +2,8 @@ export const envConfig = {
   API_URL:
     import.meta.env.MODE === 'production' ? import.meta.env.VITE_API_URL_PRODUCTION : import.meta.env.VITE_API_URL,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-  GOOGLE_REDIRECT_URI: import.meta.env.VITE_GOOGLE_REDIRECT_URI
+  GOOGLE_REDIRECT_URI:
+    import.meta.env.MODE === 'production'
+      ? import.meta.env.VITE_GOOGLE_REDIRECT_URI_PRODUCTION
+      : import.meta.env.VITE_GOOGLE_REDIRECT_URI
 }
